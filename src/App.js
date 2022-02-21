ff153a73ba39d6fc517ac312212d0ad13660eca0
Monday_David_Sarka <david_md@hamari.agency>
2022-02-21 18:32:20 +0100

    initial upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>
+            Centering an element on a page with Flex
+        </h2>
+      </header>
+      <div>
+          <h3>hello world</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
