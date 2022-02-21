3b49246a344379598393bec0e0fc0c0262d28c5d
Monday_David_Sarka <david_md@hamari.agency>
2022-02-21 20:09:01 +0100

    house cleaning

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-            Centering an element on a page with Flex
-        </h2>
-      </header>
-      <div>
-          <h3>hello world</h3>
-      </div>
-    </div>
+    <>Hello World!</>
   );
 }
 
