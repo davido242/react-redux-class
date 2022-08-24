ac7789c9c6f6084b2f57917c391eb0d708671f8e
Monday_David_Sarka <david_md@hamari.agency>
2022-08-25 00:30:16 +0100

    Managed States Locally within a class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import CheckAgeNew from './Greet';
+import DisplayMessages from './Greet';
 
 const GameOfChance = () => {
       let x = 10;
       return (
         <div>
           <h2>Thanks Daveed {"+ " + x}</h2>
-          <CheckAgeNew />
+          <DisplayMessages />
       </div>
     );
   }
