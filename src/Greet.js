57ce0f64da6b247a31c90379162adcccfcebf536
Monday_David_Sarka <david_md@hamari.agency>
2022-08-24 18:27:07 +0100

    Added a class component with its constructor and some initialised states

--- src/Greet.js
+++ src/Greet.js
@@ -1,12 +1,11 @@
-// React Redux Class begins 7th July, 2022
-import { useEffect } from 'react';
+import React from "react";
 
-function Greet({ name }) {
-  const message = `Hello, ${name}!`;
-  useEffect(() => {
-    document.title = `Greetings to ${name}`; 
-  }, [name]);
-  return <div>{message}</div>;
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      messages: []
+    }
+  }
 }
-
-export default Greet;
